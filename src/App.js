2155ddac43c81f149d9944980a456057c70ba5c5
Angelo <59510899+angeloesaki@users.noreply.github.com>
2020-04-22 10:40:36 +0900

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className = "name">
+        <h1>My name is Angelo.</h1>
+      </div>
+
+    );
+  }
+}
+export default App;
